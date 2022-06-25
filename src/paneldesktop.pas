005844a2da9ed6d5d40a1f5e20709caf5f6d8e6f
afuriza <dioaffriza@gmail.com>
2022-06-25 18:08:44 +0700

    * set transparency + screenshot file added

---
+++ src/paneldesktop.pas
@@ -0,0 +1,68 @@
+unit PanelDesktop;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, BCPanel,
+  DateUtils, x, xlib, qt5, qtwidgets;
+
+type
+
+  { TfrPanel }
+
+  TfrPanel = class(TForm)
+    pnDesktop: TBCPanel;
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  frPanel: TfrPanel;
+
+implementation
+uses
+  MainDesktop;
+
+{$R *.lfm}
+
+{ TfrPanel }
+
+procedure TfrPanel.Timer1Timer(Sender: TObject);
+begin
+  pnDesktop.Caption := DateTimeToStr(Now);
+end;
+
+procedure TfrPanel.FormShow(Sender: TObject);
+var
+  SelfWindow: TWindow;
+begin
+  SelfWindow := QWidget_winId(TQtMainWindow(Self.Handle).Widget);
+  Top := 0;
+  Left := 0;
+
+  frDesktop.util.SetDockedMode(SelfWindow);
+  Width := Screen.Width;
+  Height := 35;
+
+
+  //frDesktop.util.SetStrut(SelfWindow, Width, Height);
+  frDesktop.util.SetStrut(SelfWindow, Width, Height, 0);
+  frDesktop.util.ActivateWindow(SelfWindow);
+end;
+
+procedure TfrPanel.FormCreate(Sender: TObject);
+begin
+  QWidget_setAttribute(TQtMainWindow(Self.Handle).Widget, QtWA_TranslucentBackground);
+  QWidget_setAttribute(TQtMainWindow(Self.Handle).GetContainerWidget, QtWA_TranslucentBackground);
+end;
+
+end.
+
